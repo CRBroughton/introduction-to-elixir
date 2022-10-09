d87b0f52bca1774b9480a590278d363fd48c84c1
CRBroughton <crbroughton@posteo.uk>
2022-10-09 11:15:07 +0100

    add example of struct

--- 06-data-abstraction-function-piping-&-structs/main.ex
+++ 06-data-abstraction-function-piping-&-structs/main.ex
@@ -1,3 +1,15 @@
+defmodule Fraction do
+  defstruct x: nil, y: nil
+
+  def new(x, y) do
+    %Fraction{x: x, y: y}
+  end
+
+  def value(%Fraction{x: x, y: y}) do
+    x / y
+  end
+end
+
 defmodule TaskList do
   # returns an empty Map
   def new(), do: MultiStorage.new()
